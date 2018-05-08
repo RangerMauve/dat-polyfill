6682a3e35d9e00c4759b240d040fe00a426ab656
chrisekelley <chris@vetula.com>
2018-05-08 17:27:43 +0200

    code cleanup

--- demo/client/index.js
+++ demo/client/index.js
@@ -1,9 +1,5 @@
 async function receiveMessage (event) {
-  // Do we trust the sender of this message?
-  // if (event.origin !== "http://example.com:8080")
-  //     return;
-
-  // console.log(event.data)
+// console.log(event.data)
   let data = event.data
   if (typeof data === 'string' && data.startsWith('dat:')) {
     let datAddress = data
